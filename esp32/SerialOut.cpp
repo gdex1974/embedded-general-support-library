bc582ab90747efdc88e751b3df10fad23b686993
gdex <gdex1974@gmail.com>
2023-07-10 17:25:59 +0200

    Rename esp32 directory to esp32-arduino

--- esp32/SerialOut.cpp
+++
@@ -1,9 +0,0 @@
-#include "../SerialOut.h"
-#include "PacketUartImpl.h"
-
-void embedded::SerialOut::flush()
-{
-    auto dataView = bufferedOut.data();
-    uartDevice.Send(dataView.begin(), dataView.size(), 100);
-    bufferedOut.clear();
-}
