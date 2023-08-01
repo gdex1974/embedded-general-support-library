8b0f730721a1abea948429e9a910c60d36f058a6
gdex <gdex1974@gmail.com>
2023-08-01 16:31:34 +0200

    Make most of MemoryView functions constexpr. Add a copy constructor from nonconst to const data.

--- MemoryView.h
+++ MemoryView.h
@@ -21,27 +21,29 @@ public:
     typedef std::size_t size_type;
     typedef std::ptrdiff_t difference_type;
 
-    MemoryView() : begin_(nullptr), size_(0) {}
-    MemoryView(T* p, uint16_t s) : begin_(p), size_(s) {}
+    constexpr MemoryView() : begin_(nullptr), size_(0) {}
+    constexpr MemoryView(T* p, uint16_t s) : begin_(p), size_(s) {}
+    template<typename C = std::remove_const<T>, std::enable_if_t<std::is_same_v<T, std::add_const_t<C>>, bool> = true>
+    constexpr MemoryView(const MemoryView<C> &other) : begin_(other.begin()), size_(other.size()) {}
 
     template<std::size_t N>
-    MemoryView(std::array<T, N> &ar) : begin_(ar.begin()), size_(ar.size()) {}
+    constexpr MemoryView(std::array<T, N> &ar) : begin_(ar.begin()), size_(ar.size()) {}
 
     template<std::size_t N>
-    MemoryView(T(&ar)[N]) : begin_(ar), size_(N) {}
+    constexpr MemoryView(T(&ar)[N]) : begin_(ar), size_(N) {}
 
     template<typename C = T, std::enable_if_t<std::is_same_v<char, std::remove_const_t<C>>, bool> = true>
-    operator std::string_view() const
+    constexpr operator std::string_view() const
     {
         return { begin_, size_ };
     }
 
     T &operator[](uint16_t i) { return begin_[i]; }
-    const T &operator[](uint16_t i) const { return begin_[i]; }
+    constexpr T &operator[](uint16_t i) const { return begin_[i]; }
 
-    auto begin() const { return begin_; }
-    auto end() const { return begin_ + size_; }
-    auto size() const { return size_; }
+    constexpr auto begin() const { return begin_; }
+    constexpr auto end() const { return begin_ + size_; }
+    constexpr auto size() const { return size_; }
 
 private:
     T* begin_;
