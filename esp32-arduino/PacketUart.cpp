e7231b694a29515eaefd0d6f4078d57151e7bc12
gdex <gdex1974@gmail.com>
2023-08-27 22:03:46 +0200

    Merge interface with the master branch. Add library.json

--- esp32-arduino/PacketUart.cpp
+++ esp32-arduino/PacketUart.cpp
@@ -8,11 +8,6 @@ namespace embedded
 static_assert(sizeof(HardwareSerial) == sizeof(PacketUart::UartDevice));
 static_assert(std::is_convertible_v<PacketUart::UartDevice, HardwareSerial>);;
 
-PacketUart::PacketUart(UartDevice &uart) : uartDevice(uart)
-{
-
-}
-
 uint16_t PacketUart::Receive(uint8_t* buffer, uint16_t bufferSize, uint16_t timeout)
 {
     auto startTime = millis();
@@ -49,6 +44,24 @@ uint16_t PacketUart::Send(const uint8_t* buffer, uint16_t bufferSize, uint32_t t
 uint16_t PacketUart::ReceiveUntil(uint8_t* buffer, uint16_t bufferSize, uint8_t endByte, uint16_t timeoutMilliseconds)
 {
     uint16_t currentPos = 0;
+    const auto oldTimeout = uartDevice.getTimeout();
+    if (timeoutMilliseconds > 0)
+    {
+        uartDevice.setTimeout(timeoutMilliseconds);
+    }
+    currentPos += uartDevice.readBytesUntil((char)endByte, buffer, bufferSize - 1);
+    buffer[currentPos++] = endByte;
+    uartDevice.setTimeout(oldTimeout);
+    return currentPos;
+}
+
+uint16_t PacketUart::ReceiveBetween(uint8_t* buffer,
+                                    uint16_t bufferSize,
+                                    uint8_t startByte,
+                                    uint8_t endByte,
+                                    uint16_t timeoutMilliseconds)
+{
+    uint16_t currentPos = 0;
     auto oldTimeout = uartDevice.getTimeout();
     if (timeoutMilliseconds > 0)
     {
@@ -59,7 +72,7 @@ uint16_t PacketUart::ReceiveUntil(uint8_t* buffer, uint16_t bufferSize, uint8_t 
     while (timeoutMilliseconds == 0 || millis() < maxTime)
     {
         int nextByte = uartDevice.read();
-        if (nextByte >= 0)
+        if (nextByte == startByte)
         {
             buffer[currentPos++] = (uint8_t)nextByte;
             currentPos += uartDevice.readBytesUntil((char)endByte, buffer + currentPos, bufferSize - currentPos - 1);
