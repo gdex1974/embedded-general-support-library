76083f541cb8fa29abf7597d78fc1972e5fd8238
gdex <gdex1974@gmail.com>
2023-09-02 14:17:09 +0200

    Fix rebase and merge issues

--- esp32-esp-idf/Delays.cpp
+++ esp32-esp-idf/Delays.cpp
@@ -1,6 +1,9 @@
 #include "Delays.h"
 
-#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/portmacro.h>
+#include <esp_sleep.h>
 
 void embedded::ligntSleep(uint32_t milliseconds)
 {
@@ -23,17 +26,25 @@ void embedded::ligntSleep(uint32_t milliseconds)
 
 void embedded::delay(uint32_t milliseconds)
 {
-    ::delay(milliseconds);
+    vTaskDelay(milliseconds / portTICK_PERIOD_MS);
 }
 
 uint32_t embedded::getMillisecondTicks()
 {
-    return ::millis();
+    return esp_timer_get_time() / 1000;
 }
 
 void embedded::delayMicroseconds(uint32_t microseconds)
 {
-    ::delayMicroseconds(microseconds);
+    if (microseconds){
+        auto current = static_cast<uint64_t>(esp_timer_get_time());
+        uint64_t expected = current + microseconds;
+        if (expected < current)
+        {
+            while(static_cast<uint64_t>(esp_timer_get_time()) > expected);
+        }
+        while(static_cast<uint64_t>(esp_timer_get_time()) < expected);
+    }
 }
 
 void embedded::deepSleep(uint32_t milliseconds)
@@ -44,5 +55,5 @@ void embedded::deepSleep(uint32_t milliseconds)
 
 uint64_t embedded::getMicrosecondTicks()
 {
-    return ::micros();
+    return static_cast<uint64_t>(esp_timer_get_time());
 }
