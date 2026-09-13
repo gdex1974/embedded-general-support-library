7e4f5c0064e99ddac46cb0196c459686d8bb5120
gdex <gdex1974@gmail.com>
2026-09-13 19:27:08 +0200

    Simplify UTF-8 decoder and make it more compliant to the standard.

--- gfx-font-converter/FreeTypeGlyph.h
+++ gfx-font-converter/FreeTypeGlyph.h
@@ -9,6 +9,7 @@ public:
     explicit FreeTypeGlyph(FT_Glyph glyph) : ftGlyph(glyph, FT_Done_Glyph) {}
 
     FT_BitmapGlyphRec& getBitmapGlyph() const;
+
 private:
     using FTGlyphHandler = std::unique_ptr<FT_GlyphRec_, decltype(&FT_Done_Glyph)>;
     FTGlyphHandler ftGlyph;
--- gfx-font-converter/FreeTypeLibrary.h
+++ gfx-font-converter/FreeTypeLibrary.h
@@ -10,8 +10,14 @@ class FreeTypeLibrary
 {
 public:
     FreeTypeLibrary();
-    FT_Library get() const { return ftLibrary.get(); }
-    FreeTypeFace createFace(const std::string& fontPath, int faceIndex = 0) const;
+
+    FT_Library get() const
+    {
+        return ftLibrary.get();
+    }
+
+    FreeTypeFace createFace(const std::string &fontPath, int faceIndex = 0) const;
+
 private:
     using FTLibraryHandler = std::unique_ptr<FT_LibraryRec_, decltype(&FT_Done_FreeType)>;
     FTLibraryHandler ftLibrary;
