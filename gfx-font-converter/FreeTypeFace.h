70cee34f2ba861aec1ee527338ce6f246110848d
gdex <gdex1974@gmail.com>
2026-09-13 19:47:08 +0200

    Simplify UTF-8 decoder and make it more compliant to the standard.

--- gfx-font-converter/FreeTypeFace.h
+++ gfx-font-converter/FreeTypeFace.h
@@ -10,15 +10,10 @@ class FreeTypeFace
 {
 public:
     explicit FreeTypeFace(FT_Face face) : ftFace(face, FT_Done_Face) {}
-
-    FT_Face get() const
-    {
-        return ftFace.get();
-    }
-
+    FT_Face get() const { return ftFace.get(); }
     void setCharSize(FT_F26Dot6 width, FT_F26Dot6 height, int horzResolution, int vertResolution) const;
     FreeTypeGlyph renderGlyph(std::uint32_t symbol, int loadFlags, FT_Render_Mode renderMode) const;
-
+    double getSizeMetricHeihgt() const { return ftFace->size->metrics.height/64.; }
 private:
     using FTFaceHandler = std::unique_ptr<std::remove_pointer_t<FT_Face>, decltype(&FT_Done_Face)>;
     FTFaceHandler ftFace;
--- gfx-font-converter/FreeTypeGlyph.h
+++ gfx-font-converter/FreeTypeGlyph.h
@@ -9,7 +9,6 @@ public:
     explicit FreeTypeGlyph(FT_Glyph glyph) : ftGlyph(glyph, FT_Done_Glyph) {}
 
     FT_BitmapGlyphRec& getBitmapGlyph() const;
-
 private:
     using FTGlyphHandler = std::unique_ptr<FT_GlyphRec_, decltype(&FT_Done_Glyph)>;
     FTGlyphHandler ftGlyph;
